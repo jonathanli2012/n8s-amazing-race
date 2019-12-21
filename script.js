b12e0968b0283626e04142d2231ad41471364950
Jonathan Li <jonathanli2012@gmail.com>
2019-12-20 21:50:37 -0600

    edits

--- script.js
+++ script.js
@@ -6,26 +6,26 @@ $(function() {
     $(".outerBox").css("width", "100%");
     $(".formBox").css("width", "95%");
     $(".outputBox").css("width", "70%");
-    
+   
     $(".formBox").css("height", "40%");
     $(".outputBox").css("height", "60%");
-    
+   
     $(".formBox").css("top", "40%");
     $(".outputBox").css("top", "40%");
-    
+   
     $(".btnCheck").css("height", "40px");
-    
+   
   } else { // non-mobile device
     $(".outerBox").css("width", "90%");
     $(".formBox").css("width", "65%");
     $(".outputBox").css("width", "65%");
-    
+   
     $(".formBox").css("height", "50%");
     $(".outputBox").css("height", "50%");
-    
+   
     $(".formBox").css("top", "50%");
     $(".outputBox").css("top", "50%");
-    
+   
     $(".btnCheck").css("height", "32px");
   }
 });
@@ -35,26 +35,26 @@ $($(window).resize(function() {
     $(".outerBox").css("width", "100%");
     $(".formBox").css("width", "95%");
     $(".outputBox").css("width", "70%");
-    
+   
     $(".formBox").css("height", "40%");
     $(".outputBox").css("height", "60%");
-    
+   
     $(".formBox").css("top", "40%");
     $(".outputBox").css("top", "40%");
-    
+   
     $(".btnCheck").css("height", "40px");
-    
+   
   } else { // non-mobile device
     $(".outerBox").css("width", "90%");
     $(".formBox").css("width", "65%");
     $(".outputBox").css("width", "65%");
-    
+   
     $(".formBox").css("height", "50%");
     $(".outputBox").css("height", "50%");
-    
+   
     $(".formBox").css("top", "50%");
     $(".outputBox").css("top", "50%");
-    
+   
     $(".btnCheck").css("height", "32px");
   }
 }));
@@ -89,7 +89,7 @@ function update_vars(currentclue)
 //updates clue on button click
 $($(".btnCheck").click(function() {
   var $input = $(".input").val();
-  
+ 
   if($input == "key 1") {
     $(".outputBody").css("color", "rgb(41, 41, 41)");
     $(".outputBox").css("text-align", "left");
@@ -98,51 +98,51 @@ $($(".btnCheck").click(function() {
     currentclue = "test clue";
     update_vars(currentclue);
     showform();
-    
-  } else if($input == "I269"){
+   
+  } else if($input == "PYGCPOIW"){
     $(".outputBody").css("color", "rgb(41, 41, 41)");
     $(".outputBox").css("text-align", "left");
     $(".outputHead").text("Clue:");
-    $(".outputBody").text("Snapchat Lily (she's in the group chat) and let her know you're coming to our house. Only then will she let you in when you arrive.");
+    $(".outputBody").text("Your next clue is hiding somewhere in the Walmart nearest to here.");
     currentclue = "clue 1";
     update_vars(currentclue);    
     showform();
 
 
-  } else if($input == "e271828"){
+  } else if($input == "LookItsSanta"){
     $(".outputBody").css("color", "rgb(41, 41, 41)");
     $(".outputBox").css("text-align", "left");
     $(".outputHead").text("Clue:");
-    $(".outputBody").text("Remember Stop Sign Search? The perimeter is a bit different. Hart/Pine/Kirk/Butterfield. Have at it!");
+    $(".outputBody").text("Go to the playground of the nearest elementary school.");
     currentclue = "clue 2";
-    update_vars(currentclue);     
+    update_vars(currentclue);    
     showform();
 
-  } else if($input == "mAri0"){
+  } else if($input == "diehard"){
     $(".outputBody").css("color", "rgb(41, 41, 41)");
     $(".outputBox").css("text-align", "left");
     $(".outputHead").text("Clue:");
-    $(".outputBody").text("Behind the names of the buildings awaits your material... and although it's always 5-11 year olds, age is the most important factor.");
+    $(".outputBody").text("As usual, meet in the BHS parking lot at 10:45. Make sure you bring a pencil/pen so I don't have to provide them for certain tasks, and also your wallets. My venmo is Nathan-Wright-113; you must send me $10 by 10am tomorrow in order to participate. If both of your teammates are wearing santa hats, you will get your first clue one minute before the other teams. See you tomorrow!");
     currentclue = "clue 3";
-    update_vars(currentclue);     
+    update_vars(currentclue);    
     showform();
 
-  } else if($input == "51HHJJ"){
+  } else if($input == "ThisIsntFestive"){
     $(".outputBody").css("color", "rgb(41, 41, 41)");
     $(".outputBox").css("text-align", "left");
     $(".outputHead").text("Clue:");
-    $(".outputBody").text("Synonyms: Stake Connection Middle");
+    $(".outputBody").text("Come back to Nathan's House!");
     currentclue = "clue 4";
     update_vars(currentclue);    
     showform();
 
-  } else if($input == "t6Yq1"){
+  } else if($input == "ZXPZZ"){
     $(".outputBody").css("color", "rgb(41, 41, 41)");
     $(".outputBox").css("text-align", "left");
     $(".outputHead").text("Clue:");
-    $(".outputBody").text("Keep your cool, or else you'll get NUTTY at this park. A face-off awaits you there, but not before Pit Stop #1! Get to the next location first to win $10!");
+    $(".outputBody").text("12 Days of Christmas: Find exactly 12 of one item (example: 12 cupcakes), take a picture of it, and send it to me with the number and what it is. Then do the same for 11. Go all the way down to 1 for your next clue.");
     currentclue = "clue 5";
-    update_vars(currentclue);   
+    update_vars(currentclue);  
     showform();
 
   } else if($input == "H27Ui"){
@@ -151,7 +151,7 @@ $($(".btnCheck").click(function() {
     $(".outputHead").text("Clue:");
     $(".outputBody").text("The final stretch! I'm located somewhere within the bounds of the Stop Sign Search area! Good luck!");
     currentclue = "clue 6";
-    update_vars(currentclue );     
+    update_vars(currentclue );    
     showform();
 
   } else {
